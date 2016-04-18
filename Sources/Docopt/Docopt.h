366df8c7abf94e81789daceed0d88b8880be27b0
anhdat <mr.anhdat@gmail.com>
2016-04-18 15:53:01 +0700

    Add dependency docopt.swift

---
+++ Sources/Docopt/Docopt.h
@@ -0,0 +1,17 @@
+//
+//  Docopt.h
+//  Docopt
+//
+//  Created by Pavel S. Mazurin on 2/28/15.
+//  Copyright (c) 2015 kovpas. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for docopt.
+FOUNDATION_EXPORT double docoptVersionNumber;
+
+//! Project version string for docopt.
+FOUNDATION_EXPORT const unsigned char docoptVersionString[];
+
+
