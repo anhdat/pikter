c587d62c11875f9c89a8b57b93bb9d6dbb5db78f
anhdat <mr.anhdat@gmail.com>
2016-04-21 00:54:13 +0700

    Add width height param

--- Sources/pikter/ImagePrinting.swift
+++ Sources/pikter/ImagePrinting.swift
@@ -1,3 +1,24 @@
+/*
+Key             Description of value
+
+name        base-64 encoded filename. Defaults to "Unnamed file".
+size        File size in bytes. Optional; this is only used by the progress indicator.
+width       Width to render. See notes below.
+height      Height to render. See notes below.
+preserveAspectRatio     If set to 0, then the image's inherent aspect ratio will not be respected;
+                        otherwise, it will fill the specified width and height as much as possible
+                        without stretching. Defaults to 1.
+inline                  If set to 1, the file will be displayed inline. Otherwise, it will be
+                        downloaded with no visual representation in the terminal session.
+                        Defaults to 0.
+
+The width and height are given as a number followed by a unit, or the word "auto".
+
+N: N character cells.
+Npx: N pixels.
+N%: N percent of the session's width or height.
+auto: The image's inherent size will be used to determine an appropriate dimension.
+*/
 import Foundation
 
 
@@ -6,18 +27,53 @@ enum ImagePrintingError: ErrorType {
 }
 
 
-func printImage(data data: NSData, name: String="Unnamed") {
+func printImage(data data: NSData,
+    name: String="Unnamed",
+    height: Int?=nil, width: Int?=nil,
+    isPercent: Bool=false,
+    preserveAspectRatio: Bool=true,
+    isDebug: Bool=false
+    ) {
     let fileContent = data.base64EncodedStringWithOptions(.Encoding64CharacterLineLength)
-    print("\u{001B}]1337;File=inline=1;")
+    if !isDebug {
+        print("\u{001B}]1337;File=inline=1;" )
+    }
+
+    let unit = isPercent ? "%" : "px"
+
+    if let height = height {
+        print("height=\(height)", unit, ";", terminator:"", separator:"")
+        if width == nil {
+            print("width=auto;", terminator:"")
+        }
+    }
+    if let width = width {
+        print("width=\(width)", unit, ";", terminator:"", separator:"")
+        if height == nil {
+            print("height=auto;", terminator:"")
+        }
+    }
+    print("preserveAspectRatio=true")
     print(":")
-    print(fileContent)
-    print("\u{7}")
+    if !isDebug {
+        print(fileContent)
+        print("\u{7}")
+    }
 }
 
 
-func printImage(path path: String) throws {
+func printImage(path path: String,
+    height: Int?=nil, width: Int?=nil,
+    isPercent: Bool=false,
+    preserveAspectRatio: Bool=true,
+    isDebug: Bool=false
+    ) throws {
     guard let imageData = NSData(contentsOfFile: path) else {
         throw ImagePrintingError.InvalidFilePath(path: path)
     }
-    printImage(data:imageData)
+    printImage(data: imageData,
+        width: width, height: height,
+        isPercent: isPercent,
+        isDebug: isDebug
+        )
 }
--- Sources/pikter/main.swift
+++ Sources/pikter/main.swift
@@ -4,7 +4,7 @@ import Docopt
 let doc : String = "pikter - Picture on iTerm\n" +
 "\n" +
 "Usage:\n" +
-"  pikter <filePath>\n" +
+"  pikter <filePath> [--width=<px>, --height=<px>, --percent=<True>, (-d | --debug)]\n" +
 "  pikter (-h | --help)\n" +
 "\n" +
 "Options:\n" +
@@ -22,11 +22,38 @@ func main() {
     let terminalInput = getTerminalInput()
 
     guard let filePath = terminalInput["<filePath>"] as? String else {
+        print("Missing file path")
         return
     }
 
+    var isDebug: Bool = false
+
+    if let inputDebug = terminalInput["--debug"] as? Int {
+        isDebug = inputDebug == 1
+    }
+
+    if isDebug {
+        print("--In Debug Mode--")
+    }
+
+
+    var width: Int?
+    var height: Int?
+
+    if let inputWidth = terminalInput["--width"] as? String {
+        width = Int(inputWidth)
+    }
+    if let inputHeight = terminalInput["--height"] as? String {
+        height = Int(inputHeight)
+    }
+
+    if isDebug {
+        print(terminalInput)
+        print("width: \(width), height: \(height)")
+    }
+
     do {
-        try printImage(path: filePath)
+        try printImage(path: filePath, width: width, height: height, isDebug: isDebug)
     } catch ImagePrintingError.InvalidFilePath(let path) {
         print("Can't resolve a file from input path: \"\(path)\"")
     } catch {
